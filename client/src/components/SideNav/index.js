4e5d0e791e2096935979cdff63e2e8b73d0ee8e2
Russell Telen <rtelen@my.bcit.ca>
2021-03-14 21:06:23 -0700

    Add icons to SideNav

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -4,6 +4,14 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import { makeStyles } from "@material-ui/core/styles";
+import { ListItemIcon } from "@material-ui/core";
+
+import QueueIcon from "@material-ui/icons/Queue";
+import CameraEnhanceIcon from "@material-ui/icons/CameraEnhance";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const useStyles = makeStyles((theme) => ({
   // necessary for content to be below app bar
@@ -26,25 +34,43 @@ const SideNav = ({
       <Divider />
       <List>
         <ListItem button onClick={() => collectionsClicked()}>
+          <ListItemIcon>
+            <LibraryBooksIcon />
+          </ListItemIcon>
           <ListItemText primary="Collections" />
         </ListItem>
         <ListItem button onClick={() => camerasClicked()}>
+          <ListItemIcon>
+            <CameraEnhanceIcon />
+          </ListItemIcon>
           <ListItemText primary="Cameras" />
         </ListItem>
         <ListItem button onClick={() => addNewCollectionClicked()}>
+          <ListItemIcon>
+            <QueueIcon />
+          </ListItemIcon>
           <ListItemText primary="Add New Collection" />
         </ListItem>
         {!user ? (
           <>
             <ListItem button onClick={() => loginClicked()}>
+              <ListItemIcon>
+                <LockOpenIcon />
+              </ListItemIcon>
               <ListItemText primary="Login" />
             </ListItem>
             <ListItem button onClick={() => registerClicked()}>
+              <ListItemIcon>
+                <VpnKeyIcon />
+              </ListItemIcon>
               <ListItemText primary="Register" />
             </ListItem>
           </>
         ) : (
           <ListItem button onClick={() => logoutClicked()}>
+            <ListItemIcon>
+              <ExitToAppIcon />
+            </ListItemIcon>
             <ListItemText primary="Logout" />
           </ListItem>
         )}
