15df54e32e4bbe2fd7ee3a44b4ff80c17da3b178
Russell Telen <rtelen@my.bcit.ca>
2021-03-15 21:07:11 -0700

    Reroute to collections page after login

--- client/src/layouts/LoginPage/index.js
+++ client/src/layouts/LoginPage/index.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import LoginForm from "../../components/LoginForm";
 import useLocalStorage from "react-use-localstorage";
-import axios from "axios";
 import { loginUser } from "../../network";
 
 const LoginPage = () => {
+  const history = useHistory();
   const [token, setToken] = useLocalStorage("token");
 
   const submit = async (data) => {
     try {
       const accessToken = await loginUser(data);
-      accessToken && setToken(accessToken);
+      if (accessToken) {
+        setToken(accessToken);
+        history.push("/collections");
+        return;
+      }
     } catch (e) {
       console.log(e);
     }
