c2309a7d613ae84748721cc46688edd42b207502
Russell Telen <rtelen@my.bcit.ca>
2021-03-20 19:43:41 -0700

    Populate edit camera form w/ existing values

--- client/src/layouts/EditCameraFormPage/index.js
+++ client/src/layouts/EditCameraFormPage/index.js
@@ -1,8 +1,41 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import EditCameraForm from "../../components/EditCameraForm"
+import { useParams } from "react-router-dom"
+import axios from "axios"
 
 const EditCameraFormPage = () => {
-  return <EditCameraForm />
+  const { collectionId, cameraId } = useParams()
+  const [camera, setCamera] = useState(null)
+
+  const getCameraById = async (cameraId) => {
+    const res = await axios.get(`/api/cameras/${cameraId}`)
+    return res.data
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await getCameraById(cameraId)
+
+      setCamera(res)
+    })()
+  }, [cameraId])
+
+  useEffect(() => {
+    // console.log(camera)
+  }, [camera])
+
+  const submit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <EditCameraForm
+      collectionId={collectionId}
+      cameraId={cameraId}
+      submit={submit}
+      camera={camera}
+    />
+  )
 }
 
 export default EditCameraFormPage
