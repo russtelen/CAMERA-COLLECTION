7047aeac98da34abd755265d4de42da64cd22de9
Russell Telen <rtelen@my.bcit.ca>
2021-03-21 08:39:14 -0700

    Populate edit collection form with existing data #1

--- client/src/components/EditCollectionForm/index.js
+++ client/src/components/EditCollectionForm/index.js
@@ -1,9 +1,66 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos"
+import ArrowBackIcon from "@material-ui/icons/ArrowBack"
 
-const EditCollectionForm = () => {
+const EditCollectionForm = ({ edit, collection }) => {
+  const [title, setTitle] = useState("")
+
+  useEffect(() => {
+    setTitle(collection.title)
+  }, [collection])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    edit({ title })
+    setTitle("")
+  }
+
   return (
-    <div>
-      <h1>Edit Collection</h1>
+    <div className="container d-flex justify-content-center">
+      <div style={{ width: "30vw" }}>
+        <h1 className="text-center">Edit {title}</h1>
+        <Link to={`/collections/${collection._id}`}>
+          <ArrowBackIcon />
+          Cancel
+        </Link>
+        <div className="my-5">
+          <form>
+            <div>
+              <div className="row">
+                <div className="col-12 my-2">
+                  <TextField
+                    type="text"
+                    id="standard-basic"
+                    label="Collection Name"
+                    fullWidth
+                    color="secondary"
+                    margin="dense"
+                    required
+                    variant="filled"
+                    value={title || ""}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+                <div className="col-12 d-flex justify-content-center my-2">
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    size="large"
+                    endIcon={<AddToPhotosIcon />}
+                    fullWidth
+                    onClick={handleSubmit}
+                  >
+                    Edit
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/layouts/EditCollectionFormPage/index.js
+++ client/src/layouts/EditCollectionFormPage/index.js
@@ -1,8 +1,26 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import EditCollectionForm from "../../components/EditCollectionForm"
+import { useParams } from "react-router-dom"
+import { getCollectionById } from "../../network"
 
 const EditCollectionFormPage = () => {
-  return <EditCollectionForm />
+  const [collection, setCollection] = useState("")
+
+  const { collectionId } = useParams()
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await getCollectionById(collectionId)
+      setCollection(res)
+    })()
+  }, [collectionId])
+
+  useEffect(() => {}, [collection])
+
+  const edit = (data) => {
+    console.log(data)
+  }
+  return <EditCollectionForm edit={edit} collection={collection} />
 }
 
 export default EditCollectionFormPage
