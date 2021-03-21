c296b2631215e29ab17f56a11d795f4a88744dca
Russell Telen <rtelen@my.bcit.ca>
2021-03-21 12:21:39 -0700

    Set up dotenv #4

--- server/models/db.js
+++ server/models/db.js
@@ -1,23 +1,26 @@
 // ==========
 // REQUIRE
 // ==========
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// Read .env file
+dotenv.config()
 
 // Connect Mongoose
-let localDb = process.env.localDb;
-let atlasDb = process.env.db;
+let localDb = process.env.localDb
+let atlasDb = process.env.db
 
 module.exports.connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/camera-collection", {
+    await mongoose.connect(localDb, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
-    });
+    })
 
-    console.log("MONGO CONNECTED");
+    console.log("MONGO CONNECTED")
   } catch (e) {
-    console.log("MONGO CONNECTION ERROR :(");
-    console.log(e);
+    console.log("MONGO CONNECTION ERROR :(")
+    console.log(e)
   }
-};
+}
--- server/server.js
+++ server/server.js
@@ -1,33 +1,37 @@
 // ==========
 // REQUIRE
 // ==========
-const express = require("express");
-const { connectDb } = require("./models/db");
-const session = require("express-session");
+const express = require("express")
+const { connectDb } = require("./models/db")
+const session = require("express-session")
+const dotenv = require("dotenv")
 
 // REQUIRE-AUTH
 //---------------
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/Users");
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/Users")
 // REQUIRE-ROUTERS
 //---------------
-const collectionRoutes = require("./routes/collections");
-const userRoutes = require("./routes/users");
-const cameraRoutes = require("./routes/cameras");
+const collectionRoutes = require("./routes/collections")
+const userRoutes = require("./routes/users")
+const cameraRoutes = require("./routes/cameras")
 
 // ==========
 // CONFIG
 // ==========
+// Read .env file
+dotenv.config()
+
 // init express
-const app = express();
+const app = express()
 
 // Parsing Middlewares
-app.use(express.urlencoded({ extended: true })); // application/x-www-form-urlencoded
-app.use(express.json()); // JSON
+app.use(express.urlencoded({ extended: true })) // application/x-www-form-urlencoded
+app.use(express.json()) // JSON
 
 // Connect to db
-connectDb();
+connectDb()
 
 // Express Session
 const sessionConfig = {
@@ -39,43 +43,43 @@ const sessionConfig = {
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // expires in 1 week
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
-};
-app.use(session(sessionConfig));
+}
+app.use(session(sessionConfig))
 
 // Passport/Auth
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 // ==========
 // ROUTES
 // ==========
-app.use("/api/collections", collectionRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/cameras", cameraRoutes);
+app.use("/api/collections", collectionRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/cameras", cameraRoutes)
 
 //==============================================
 // Error Handlers
 // =============================================
 app.all("*", (req, res, next) => {
-  next(new ExpressError("Page not found", 404));
-});
+  next(new ExpressError("Page not found", 404))
+})
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
+  const { statusCode = 500 } = err
   if (!err.message) {
-    err.message = "Oh no, Something went wrong";
+    err.message = "Oh no, Something went wrong"
   }
-  res.status(statusCode).send({ error: err.message, status: statusCode });
-});
+  res.status(statusCode).send({ error: err.message, status: statusCode })
+})
 
 // ==========
 // LISTEN
 // ==========
-const port = 5000 || process.env.PORT;
+const port = 5000 || process.env.PORT
 
 app.listen(port, () => {
-  console.log(`Connected to port ${port}`);
-});
+  console.log(`Connected to port ${port}`)
+})
